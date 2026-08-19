758490e8dc7e0dbf69e5b8ec8eda728ae7ff90a4
azrilmustofa <m.azrilmustofa@gmail.com>
2026-08-19 12:52:05 +0700

    api

--- app/src/main/java/microdata/absensi/data/remote/RetrofitClient.kt
+++ app/src/main/java/microdata/absensi/data/remote/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.0.7:9500/"
+    private const val BASE_URL = "https://api.microdata.co.id/presensi/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
